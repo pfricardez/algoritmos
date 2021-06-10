b42f07dded2f0939d4e6d25fd7c5d080110d5fe3
Perla Ricardez <pfricardez@gmail.com>
2021-06-10 10:49:30 -0500

    Modificaciones sobre BFS

--- src/main/java/io/sewsol/algoritmos/BreadthFirstSearch.java
+++ src/main/java/io/sewsol/algoritmos/BreadthFirstSearch.java
@@ -38,4 +38,44 @@ public class BreadthFirstSearch {
 
     }
 
+    public int getShortPath(Graph graph, Integer origin, Integer target) {
+
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.add(origin);
+
+        Deque<Integer> levels = new ArrayDeque<>();
+        levels.add(0);
+
+        Set<Integer> visited = new HashSet<>(graph.getTotalVertex());
+
+        Integer inspected = null;
+
+        while( (inspected = deque.poll()) != null ) {
+
+            int currentLevel = levels.poll();
+            
+            if ( !visited.contains(inspected) ) {
+
+                visited.add(inspected);
+
+                if (inspected.equals(target))   {
+                    return currentLevel;
+                }
+                
+                List<Integer> neibor = graph.getVertexChild(inspected);
+                deque.addAll(neibor);
+
+                currentLevel++;
+                for (Integer nei : neibor) {
+                    levels.add(currentLevel);
+                }    
+                
+            }
+
+        }
+
+        return -1;
+
+    }
+
 }
--- src/test/java/io/sewsol/algoritmos/BreathFirstSearchTest.java
+++ src/test/java/io/sewsol/algoritmos/BreathFirstSearchTest.java
@@ -39,10 +39,11 @@ public class BreathFirstSearchTest {
 
         Graph grafo = new Graph(5, List.of( List.of(1, 2), List.of(1, 3), List.of(2, 4), List.of(4, 5), List.of(3, 5) ));
 
-        boolean result = algoritmo.search(grafo, 1, 5);
+        int result = algoritmo.getShortPath(grafo, 1, 5);
 
-        assertEquals(true, result, "Camino mas corto");
+        assertEquals(2, result, "Camino mas corto");
     }
+
 
 
     
